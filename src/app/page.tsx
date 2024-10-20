c4c4ff3b26b6dfb5c37932734009e800b8b610e6
Tertha Dev Sarker <devteertha28@gmail.com>
2024-10-20 17:37:51 +0600

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <span className='border-2 border-indigo-600'>Hello World</span>
+    </div>
+  );
+}
